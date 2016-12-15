9eec9a5f9d4a9641ad3c74cadd344f7eb4c7799d
anand80 <anandratnam@live.com.my>
2016-12-16 01:41:28 +0800

    Add project files.

---
+++ IntroducingCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroducingCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.ReadLine();
+                Console.WriteLine("Hello World");
+
+            }
+        }
+    }
+}
